427382f0bbc3cbabcf605b317f8fe5578ade1978
Marcos Yuri <marcos.yuriro@gmail.com>
2025-07-07 11:39:33 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+/*alert ("Boas-vindas ao Jogo do Número Secreto!");
+let numeroSecreto = 29;
+let chute = prompt ("Escolha um número entre 1 e 30");
+
+if(chute == numeroSecreto) {
+    console.log ("Isso aí! Você descobriu o número secreto!")
+}*/
+
+/*alert ("Boas-vindas ao nosso site!");
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+let mensagemDeErro = "Erro! Preencha todos os campos";
+ alert (mensagemDeErro);*/
+ 
+ let nome = prompt ("Digite o seu nome:")
+ let idade = prompt ("Digite a sua idade:")
+
+ if (idade >= 18) {
+    alert ("Pode tirar a habilitação!")
+ }

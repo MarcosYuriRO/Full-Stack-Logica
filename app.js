093374d6fe1bae3bac7b03a62fcefd9b005e7e96
Marcos Yuri <marcos.yuriro@gmail.com>
2025-07-11 11:54:48 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 // Projeto base Alura
-/*alert("Boas-vindas ao Jogo do Número Secreto!");
+alert("Boas-vindas ao Jogo do Número Secreto!");
 let numeroSecreto = 29;
+console.log (numeroSecreto)
 let chute = prompt("Escolha um número entre 1 e 30");
 
 if (chute == numeroSecreto) {
-    console.log("Isso aí! Você descobriu o número secreto!")
+    console.log("Isso aí! Você descobriu o número secreto!");
 } else {
-    alert("Você errou! O Número Secreto era " + numeroSecreto)
-}
+    if (chute > numeroSecreto) {
+        alert ("O Número Secreto é menor que " + chute);
+    } else {
+        alert ("O Número Secreto é maior que " + chute);
+    }
+};
 
 // Desafio Tópico 1 Alura (01 ao 07)
 /*alert ("Boas-vindas ao nosso site!");
@@ -24,39 +29,36 @@ let idade = prompt ("Digite a sua idade:");
 
 if (idade >= 18) {
    alert ("Pode tirar a habilitação!");
-}*/
+};*/
 
 //Desafio Tópico 2 (01)
 /*let diaSemana = prompt ("Escreva o dia da semana");
-if (diaSemana == "Domingo") {
-   alert ("Bom fim de semana!")
-} 
-else if (diaSemana == "Sabado") {
-    alert ("Bom fim de semana!")
-} else {
-   alert ("Boa semana!")
-    }*/
+if (diaSemana == "Domingo" || diaSemana == "Sabado") {
+   alert ("Bom fim de semana!");
+}  else {
+   alert ("Boa semana!");
+    };*/
 
 //Desafio Tópico 2 (02)
 /*let numero = prompt ("Informe um número");
 if (numero >= 0) {
-    alert (numero + " é positivo")
+    alert (numero + " é positivo");
 } else {
-    alert (numero + " é negativo")
-}*/
+    alert (numero + " é negativo");
+};*/
 
 //Desafio Tópico 2 (03)
 /*let pontuacao = prompt ("Qual foi a sua pontuação?");
 if (pontuacao >= 100) {
-    alert ("Parabéns, você ganhou!")
+    alert ("Parabéns, você ganhou!");
 } else {
-    alert ("Tente novamente para ganhar.")
-}*/
-
+    alert ("Tente novamente para ganhar.");
+};*/
+ 
 //Desafio Tópico 2 (04)
 /*let saldoConta = 100;
 alert (`Você tem ${saldoConta} reais em sua conta.`);*/
 
 //Desafio Tópico 2 (05)
 /*let nome = prompt ("Digite o seu nome:");
-alert ("Boas vindas " + nome);*/
+alert ("Boas vindas, " + nome);*/

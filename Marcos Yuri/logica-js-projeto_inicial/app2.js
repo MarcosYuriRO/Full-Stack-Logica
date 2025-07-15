c9bbcbeabeba2d7fe8660fffe5803fbab9e1b630
Marcos Yuri <marcos.yuriro@gmail.com>
2025-07-15 08:45:07 -0300

    Add files via upload

---
+++ Marcos Yuri/logica-js-projeto_inicial/app2.js
@@ -0,0 +1,74 @@
+//Desafio Tópico 4 (01)
+/*console.log ("Boas-vindas, usuário(a)");*/
+
+//Desafio Tópico 4 (02)
+/*let nome = "Yuri";
+console.log ("Olá, " + nome);*/
+
+//Desafio Tópico 4 (03)
+/*let nome = "Yuri";
+alert ("Olá, " + nome);*/
+
+//Desafio Tópico 4 (04)
+/*let lingFav = prompt ("Qual sua linguagem favorita de programação?");
+console.log (lingFav);*/
+
+//Desafio Tópico 4 (05)
+/*let valor1 = 1;
+let valor2 = 4;
+let resultado = valor1 + valor2;
+console.log (`A soma de ${valor1} e ${valor2} é igual a ${resultado}`);*/
+
+//Desafio Tópico 4 (06)
+/*let valor1 = 5;
+let valor2 = 3;
+let resultado = valor1 - valor2;
+console.log (`A diferença entre ${valor1} e ${valor2} é igual a ${resultado}`);*/
+
+//Desafio Tópico 4 (07)
+/*let idade = prompt ("Insira a sua idade");
+
+if (idade >= 18) {
+    console.log ("Você é maior de idade");
+} else {
+    console.log ("Você é menor de idade");
+}*/
+
+//Desafio Tópico 4 (08)
+/*let numero  = prompt ("Digite um Número Inteiro");
+
+if (numero != 0) {
+    let sinalNumero = numero > 0 ? "positivo" : "negativo"
+    console.log (`${numero} é um número ${sinalNumero}`);
+} else {
+    console.log ("o seu número é Zero");
+};*/
+
+//Desafio Tópico 4 (09)
+/*let contador = 1
+
+while (contador <= 10) {
+    console.log (contador);
+    contador++;
+};*/
+
+//Desafio Tópico 4 (10)
+/*let nota = 10;
+
+if (nota >= 7) {
+    console.log ("Parabéns! Você foi Aprovado.");
+} else{
+    console.log ("Você foi Reprovado.");
+};*/
+
+//Desafio Tópico 4 (11)
+/*let numeroAleatorio = (Math.random());
+console.log (numeroAleatorio);*/
+
+//Desafio Tópico 4 (12)
+/*let numeroAleatorio = parseInt(Math.random() * 10 + 1);
+console.log (numeroAleatorio);*/
+
+//Desafio Tópico 4 (13)
+let numeroAleatorio = parseInt(Math.random() * 1000 + 1);
+console.log (numeroAleatorio);
